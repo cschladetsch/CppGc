068f769100465380f2682ef2e7482824b7618c5c
christian.schladetsch <christian.schladetsch@gmail.com>
2025-01-29 22:18:45 +1100

    Remove some logging so bencmarks are more comparative.

--- src/GC.cpp
+++ src/GC.cpp
@@ -6,20 +6,20 @@ GC& GC::instance() {
 }
 
 void GC::register_object(Object* obj, Generation gen) {
-    spdlog::info("Registering object: {} in generation {}", fmt::ptr(obj), static_cast<int>(gen));
+    //spdlog::info("Registering object: {} in generation {}", fmt::ptr(obj), static_cast<int>(gen));
     instance().generations[static_cast<int>(gen)].insert(obj);
 }
 
 void Object::release_ref() {
     if (--ref_count == 0) {
-        spdlog::debug("Releasing object: {}", fmt::ptr(this));
+        //spdlog::debug("Releasing object: {}", fmt::ptr(this));
         GC::remove_object(this);
         delete this;
     }
 }
 
 void GC::remove_object(Object* obj) {
-    spdlog::info("Removing object from GC: {}", fmt::ptr(obj));
+    //spdlog::info("Removing object from GC: {}", fmt::ptr(obj));
     for (auto& gen : instance().generations) {
         gen.erase(obj);
     }
@@ -34,7 +34,7 @@ void GC::collect() {
         for (auto it = gen.begin(); it != gen.end();) {
             Object* obj = *it;
             if (obj->ref_count == 0) {
-                spdlog::warn("Collecting garbage object: {}", fmt::ptr(obj));
+                //spdlog::warn("Collecting garbage object: {}", fmt::ptr(obj));
                 it = gen.erase(it);
             } else {
                 ++it;
@@ -45,7 +45,7 @@ void GC::collect() {
 }
 
 void GC::cleanup() {
-    spdlog::info("Cleaning up all objects...");
+    //spdlog::info("Cleaning up all objects...");
     for (auto& gen : instance().generations) {
         for (Object* obj : gen) {
             delete obj;
